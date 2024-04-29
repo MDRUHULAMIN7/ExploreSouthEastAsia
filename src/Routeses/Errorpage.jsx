9da7f72b0f20788486ab15d4b5e44f613e48932f
MDRUHULAMIN7 <ruhulthisis@gmail.com>
2024-04-29 11:12:52 +0600

    error page added

--- src/Routeses/Errorpage.jsx
+++ src/Routeses/Errorpage.jsx
@@ -3,12 +3,8 @@ import { Link } from "react-router-dom";
 
 const Errorpage = () => {
     return (
-        <div>
-            <div className="text-center space-y-4 mt-10">
-                <h1></h1>
-            <h1 className="text-accent text-3xl md:text-4xl lg:text-5xl">404/Page Not Found</h1>
-            <h1><Link to={'/'} className="btn btn-accent rounded-2xl">Go Back Home</Link></h1>
-            </div>
+        <div className="bg-[url('https://i.ibb.co/3kLNnrk/360-F-102439501-R8m5s-Bop-RQPF377-WQebz9u8o1-YBf-EWYW.jpg')] h-screen mx-10 bg-no-repeat bg-cover text-center  justify-center items-center flex"><h1><Link to={'/'} className="btn btn-accent rounded-2xl">Go Back Home</Link></h1>
+          
             
         </div>
     );
